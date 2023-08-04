ff8a8bf41a95a5eba1b0290db964938aa313f385
Hugo Herrera <96592188+HugoHerreraDev@users.noreply.github.com>
2023-08-04 18:14:17 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        plublicPath: "/",
+        plublicPath: "./",
     },
     resolve: {
         extensions: ['.js', '.jsx'],
